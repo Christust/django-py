724ca605f9ab285cbb393aff320546057058f47e
Christos Marroquin <christos.marroquin@hotmail.com>
2023-04-10 21:57:49 -0600

    create_product

--- apps/products/api/serializers/product_serializer.py
+++ apps/products/api/serializers/product_serializer.py
@@ -1,8 +1,25 @@
-from products.models import Product
+from apps.products.models import Product
+from apps.products.api.serializers.general_serializer import (
+    MeasureUnitSerializer,
+    CategoryProductSerializer,
+)
 from rest_framework import serializers
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    # measure_unit = serializers.StringRelatedField()
+    # category = serializers.StringRelatedField()
+
     class Meta:
         model = Product
-        exclude = ["state"]
+        exclude = ["state", "created", "updated", "deleted"]
+
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "measure_unit": instance.measure_unit.description,
+            "category": instance.category.description,
+            "name": instance.name,
+            "description": instance.description,
+            "image": instance.image if instance.image != "" else "",
+        }
--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -4,9 +4,15 @@ from apps.products.api.views.general_views import (
     CategoryProductListAPIView,
     IndicatorListAPIView,
 )
+from apps.products.api.views.product_views import (
+    ProductListAPIView,
+    ProductCreateAPIView,
+)
 
 urlpatterns = [
     path("measure_units", MeasureUnitListAPIView.as_view(), name="list_measure_units"),
     path("categories", CategoryProductListAPIView.as_view(), name="list_categories"),
     path("indicators", IndicatorListAPIView.as_view(), name="list_indicators"),
+    path("product", ProductListAPIView.as_view(), name="list_product"),
+    path("create_product", ProductCreateAPIView.as_view(), name="create_product"),
 ]
